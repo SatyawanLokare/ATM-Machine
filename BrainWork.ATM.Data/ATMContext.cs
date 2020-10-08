b9b4fe6d2ff0bf4fa10e68ad104d7f50e2724978
SatyawanLokare <satyawanlokare@gmail.com>
2020-10-08 16:44:30 +0530

    commited User database

--- BrainWork.ATM.Data/ATMContext.cs
+++ BrainWork.ATM.Data/ATMContext.cs
@@ -7,13 +7,28 @@ namespace BrainWork.ATM.Data
 {
     public class ATMContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
         {
-            string connectionString = "Data Source=DESKTOP-4QP868O\\SQLEXPRESS;Initial Catalog=BrainWork;Integrated Security=True";
+            string connectionString = "Data Source=DESKTOP-4QP868O\\SQLEXPRESS;Initial Catalog=ATMproject;Integrated Security=True";
 
             OptionsBuilder.UseSqlServer(connectionString);
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().Property(b => b.FirstName).HasColumnType("varchar(200)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.lastName).HasColumnType("varchar(200)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Address1).HasColumnType("varchar(500)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Address2).HasColumnType("varchar(500)");
+            modelBuilder.Entity<User>().Property(b => b.Landmark).HasColumnType("varchar(100)");
+            modelBuilder.Entity<User>().Property(b => b.City).HasColumnType("varchar(50)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.State).HasColumnType("varchar(50)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Pincode).HasColumnType("varchar(6)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Mobile).HasColumnType("varchar(20)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Email).HasColumnType("varchar(200)").IsRequired();
+            modelBuilder.Entity<User>().Property(b => b.Status).HasColumnType("varchar(20)");
+        }
 
     }
 }
---
+++ BrainWork.ATM.Data/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrainWork.ATM.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Landmark { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+
+    }
+}
